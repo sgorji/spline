b311d7ab74c8f053597b9e44afe2d9f9d28e6187
sgorji <saeed.gorji@proton.me>
2023-03-16 20:57:02 +0100

    Interpolation function

--- spline.py
+++ spline.py
@@ -31,6 +31,29 @@ def binary_search(x):
             left = middle + 1
         else:
             right = middle - 1
-    return left
+    return middle
 
 # interpolation function
+
+
+def interp(x):
+    position = binary_search(x)
+
+    # the position variable pinpoints the location of x among the knots
+    # now depending on the relation of the knots[position] with x
+    # the algorithm decides the value for x
+    # for edge cases where x is smaller (larger) than all the knots its
+    # value mimics the closest knot, otherwise, it returns a linear
+    # interpolation of the values of its adjacent knots
+    if x > knots[position][0]:
+        if position >= (len(knots) - 1):
+            return knots[-1][1]
+        else:
+            diff = knots[position + 1] - knots[position]
+            return knots[position][1] + diff[1] * (x - knots[position][0]) / diff[0]
+    else:
+        if position <= 0:
+            return knots[0][1]
+        else:
+            diff = knots[position] - knots[position - 1]
+            return knots[position - 1][1] + diff[1] * (x - knots[position - 1][0]) / diff[0]
