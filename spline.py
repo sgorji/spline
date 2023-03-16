abfd637e6639eb086a81d4d8a1fa121660ade69e
sgorji <saeed.gorji@proton.me>
2023-03-16 20:19:39 +0100

    Binary search

--- spline.py
+++ spline.py
@@ -10,4 +10,27 @@ knots = knots[sort_index]
 
 # binary search
 
+
+# def binary_search(left, right, x):
+#     if (right <= left + 1):
+#         return left
+#     middle = (left + right) // 2
+#     if (x > knots[middle][0]):
+#         return binary_search(middle, right, x)
+#     else:
+#         return binary_search(left, middle, x)
+
+
+def binary_search(x):
+    left = 0
+    right = len(knots) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+        if (x > knots[middle][0]):
+            left = middle + 1
+        else:
+            right = middle - 1
+    return left
+
 # interpolation function
