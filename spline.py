dbce59f4e7f2313e4dd07d7e60e32ae28c890d80
sgorji <saeed.gorji@proton.me>
2023-03-16 19:17:20 +0100

    Reading and sorting the input

--- spline.py
+++ spline.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 
 # reading input knots
+knots = np.loadtxt('knots.txt')
 
-# sorting input knots (if necessary)
+# sorting input knots by their x value (if necessary)
+sort_index = np.argsort(knots[:, 0])
+knots = knots[sort_index]
 
 # binary search
 
